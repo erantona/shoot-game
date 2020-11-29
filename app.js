9033260684899e7ef8f5ae0c2909cf00eefec94f
erantona <roysuvendu1557@gmail.com>
2020-11-29 23:34:42 +0530

    20

---
+++ app.js
@@ -0,0 +1,56 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+  constructor(x, y, radious, color) {
+    this.x = x;
+    this.y = y;
+    this.radious = radious;
+    this.color = color;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radious, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+}
+class Projectile {
+  constructor(x, y, radious, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radious = radious;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radious, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+const player = new Player(x, y, 30, 'blue');
+player.draw();
+const projectiles = [];
+
+function animate() {
+  requestAnimationFrame(animate);
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+}
+
+addEventListener('click', (event) => {
+  projectiles.push(new Projectile(x, y, 5, '#333', { x: 1, y: 1 }));
+});
+animate();
